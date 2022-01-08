30c7a8079619d598df22e7a87ea5886cb30f3ca5
Anton Kot <dr.kottok@yandex.ru>
2022-01-08 12:03:00 +0300

    react 1.3

--- my-app/src/Components/Nav.jsx
+++ my-app/src/Components/Nav.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import classes from './Nav.module.css';
-/*
-let classes = {
-'nav' :
-}
-
-let c1 = w
-*/
+console.log(classes);
 const Nav = () => {
     return <nav className={classes.nav}>
         <div className={classes.my_parametr}>
             <a>xava + post</a>
 
         </div>
-        <div className='Nav_my_parametr__Ody95'>My post</div>
-        <div className='Nav_my_parametr__Ody95'><a>New post</a></div>
-        <div className='Nav_my_parametr__Ody95'><a>post 1</a></div>
-        <div className='Nav_my_parametr__Ody95'><a>Makar</a></div>
+        <div className={classes.my_parametr}>My post</div>
+        <div className='my_parametr'><a>New post</a></div>
+        <div className='my_parametr'><a>post 1</a></div>
+        <div className={`${classes.nav} ${classes.my_parametr}`}><a>Makar</a></div>
 
 
     </nav>
