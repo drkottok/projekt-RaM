7c87445a2594f59ca979dd75e9f953808c0cbd3a
Anton Kot <dr.kottok@yandex.ru>
2022-01-14 15:20:41 +0300

    react 1.8

--- my-app/src/Components/Nav/Nav.jsx
+++ my-app/src/Components/Nav/Nav.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import kottok from './Nav.module.css';
 import {NavLink} from "react-router-dom";
+import kot from "./Nav.module.css";
 
 const Nav = () => {
     return <nav className={kottok.nav}>
-        <div className={kottok.my_parametr}>
-            <div><NavLink to="/profile" activeclassname={kottok.activeLinc}>profile</NavLink></div>
-            <div><NavLink to="/dialogs">dialogs</NavLink></div>
-            <div><NavLink to="/message">message</NavLink></div>
+        <div><NavLink to="/profile" className={navData => navData.isActive ? kottok.active : kottok.item}>Profile</NavLink>
         </div>
-
+        <div><NavLink to="/dialogs" className = {navData => navData.isActive ? kottok.active : kottok.item}>dialogs</NavLink>
+        </div>
+        <div><NavLink to="/message" className = {navData => navData.isActive ? kottok.active : kottok.item}>message</NavLink>
+        </div>
     </nav>
 }
 
